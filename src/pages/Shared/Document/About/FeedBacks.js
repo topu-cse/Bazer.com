820fc6064eee94d6c3bb18f358635cd20c920fb8
topu12345 <topu15-3604@diu.edu.bd>
2023-09-11 20:59:23 +0600

    about

--- src/pages/Shared/Document/About/FeedBacks.js
+++ src/pages/Shared/Document/About/FeedBacks.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import Slider from "react-slick";
+import img1 from '../../../../assets/about/i.jpg'
 
 const FeedBacks = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 2000,
+        cssEase: "linear"
+      };
     return (
         <div>
             <div className='text-center'>
@@ -8,7 +20,100 @@ const FeedBacks = () => {
                 <h1 className='text-4xl font-bold'>Clients Feedbacks</h1>
             </div>
 
+                  {/* slider */}
+
+                  <div className='my-[50px] px-[40px] lg:px-[10px]'>
+        
+        <Slider {...settings}>
+
+          <div>
+            <div className='flex  '>
+            <div>
+              <img  className='mt-3 h-[100px]' src={img1} alt="" />
+            </div>
+            <div className='px-5  '>
+                <p className='text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                      <p className='font-bold'>Topu Raihan</p>
+                      <p className='text-green-500 italic'>Founder</p>
+            </div>
+            </div>
             
+          </div>
+
+          <div>
+            <div className='flex  '>
+            <div>
+            <img  className='mt-3 h-[100px]' src={img1} alt="" />
+            </div>
+            <div className='px-5  '>
+                <p className='text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                      <p className='font-bold'>Topu Raihan</p>
+                      <p className='text-green-500 italic'>Founder</p>
+            </div>
+            </div>
+            
+          </div>
+
+
+          <div>
+            <div className='flex  '>
+            <div>
+            <img  className='mt-3 h-[100px]' src={img1} alt="" />
+            </div>
+            <div className='px-5  '>
+                <p className='text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                      <p className='font-bold'>Topu Raihan</p>
+                      <p className='text-green-500 italic'>Founder</p>
+            </div>
+            </div>
+            
+          </div>
+
+
+         <div>
+            <div className='flex  '>
+            <div>
+            <img  className='mt-3 h-[100px]' src={img1} alt="" />
+            </div>
+            <div className='px-5  '>
+                <p className='text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                      <p className='font-bold'>Topu Raihan</p>
+                      <p className='text-green-500 italic'>Founder</p>
+            </div>
+            </div>
+            
+          </div>
+
+
+           <div>
+            <div className='flex  '>
+            <div>
+            <img  className='mt-3 h-[100px]' src={img1} alt="" />
+            </div>
+            <div className='px-5  '>
+                <p className='text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                      <p className='font-bold'>Topu Raihan</p>
+                      <p className='text-green-500 italic'>Founder</p>
+            </div>
+            </div>
+            
+          </div>
+
+          <div>
+            <div className='flex  '>
+            <div>
+            <img  className='mt-3 h-[100px]' src={img1} alt="" />
+            </div>
+            <div className='px-5  '>
+                <p className='text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                      <p className='font-bold'>Topu Raihan</p>
+                      <p className='text-green-500 italic'>Founder</p>
+            </div>
+            </div>
+            
+          </div>
+        </Slider>
+      </div>
         </div>
     );
 };
