6b4be68ec5f0aa1ddde4a0c4206b6839d4a4f082
topu12345 <topu15-3604@diu.edu.bd>
2023-09-12 02:22:51 +0600

    modal

--- src/pages/Home/HomeProducts/CartProduct.js
+++ src/pages/Home/HomeProducts/CartProduct.js
@@ -5,18 +5,19 @@ const CartProduct = ({modalproducts}) => {
         return null; // Return early or display an error message
       }
     
-      const { name, price,img } = modalproducts;
+      const { name, price,img,id } = modalproducts;
     return (
         <div>
             <>
             <input type="checkbox" id="booking-modal" className="modal-toggle" />
-            <div className="modal">
-                <div className="modal-box relative">
+            <div className="modal  ">
+                <div className="modal-box relative ">
                     <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <img src={img}alt="" />
+                    <img    src={img}alt="" />
                     <div className='text-center'>
                     <h3 className="text-lg font-bold">{name}</h3>
                     <h3 className="text-lg font-bold">price: ${price}</h3>
+                    <h3 className="text-lg font-bold">id:{id}</h3>
                     <button  className='border-solid border-[1px] border-black-200 
                            px-[30px] mt-3  py-4 bg-green-500
                                '>Add Cart</button>
